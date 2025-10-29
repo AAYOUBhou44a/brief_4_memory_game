151989c69fe8ac495ee0a29670e43f0ccb80b71e
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-10-29 21:34:54 +0100

    Chaque carte est retournable via un clic et affiche une image

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const container = document.querySelector(".container");
 for(let j=0; j<2; j++){
 for(let i=1; i<9; i++){
-
 let carte = document.createElement("div")
 carte.classList.add("carte")
 let image = `url('img/${i}.png')`;
-let dos =  "url(img/2.png)";
+let dos =  "url(img/dos.png)";
 
 carte.style.backgroundImage = dos;
 carte.style.backgroundSize = "cover";
@@ -14,12 +13,8 @@ carte.style.backgroundRepeat = "no-repeat";
 container.appendChild(carte);
 
 carte.addEventListener("click", function(){
-    console.log("tu as clické sur moi");
+    carte.style.transform = "rotateY(180deg)"
     carte.style.backgroundImage = image;
-    if(carte != carteClicke){
-        carte.style.backgroundImage = dos;
-    }
 });
-let carteClicke = carte;
 
 }}

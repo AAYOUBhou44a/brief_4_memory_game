afa6ee28fafb161ab762d5e6ab647c286e98d201
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-10-29 22:36:02 +0100

    Les paires identiques restent visibles, les autres se retournent après un court délai.

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 const container = document.querySelector(".container");
-for(let j=0; j<2; j++){
-for(let i=1; i<9; i++){
-let carte = document.createElement("div")
-carte.classList.add("carte")
-let image = `url('img/${i}.png')`;
-let dos =  "url(img/dos.png)";
+let cartes = [];
+let premiereCarte;
+let deuxiemeCarte;
 
-carte.style.backgroundImage = dos;
-carte.style.backgroundSize = "cover";
-carte.style.backgroundPosition = "center";
-carte.style.backgroundRepeat = "no-repeat";
-container.appendChild(carte);
+for(let j = 0; j < 2; j++){
+  for(let i = 1; i < 9; i++){
+    let carte = document.createElement("div");
+    carte.classList.add("carte");
 
-carte.addEventListener("click", function(){
-    carte.style.transform = "rotateY(180deg)"
-    carte.style.backgroundImage = image;
-});
+    carte.dataset.image = `img/${i}.png`;
+    carte.style.backgroundImage = "url('img/dos.png')";
+    carte.style.backgroundSize = "cover";
+    carte.style.backgroundPosition = "center";
+    carte.style.backgroundRepeat = "no-repeat";
 
-}}
+    container.appendChild(carte);
+    cartes.push(carte);
+
+    // on fait l'événement dans le boucle car chaque carte doit avoir son propre événement 
+    carte.addEventListener("click", function() {
+      if (carte.style.backgroundImage.includes("dos.png")) {
+        carte.style.backgroundImage = `url('${carte.dataset.image}')`;
+
+        if (!premiereCarte) {
+          premiereCarte = carte;
+        } else {
+          deuxiemeCarte = carte;
+
+          if (premiereCarte.dataset.image === deuxiemeCarte.dataset.image) {
+            premiereCarte = null;
+            deuxiemeCarte = null;
+          } else {
+            setTimeout(function() {
+              premiereCarte.style.backgroundImage = "url('img/dos.png')";
+              deuxiemeCarte.style.backgroundImage = "url('img/dos.png')";
+              premiereCarte = null;
+              deuxiemeCarte = null;
+            }, 800);
+          }
+        }
+      }
+    });
+  }
+}
+
+// Mélange les cartes
+cartes.sort(() => Math.random() - 0.5);
+cartes.forEach(carte => container.appendChild(carte));
